48ecbf59979c10c3e8c717a39cbc12edf2f7b3c3
freetimezp <kraftukrnet@ukr.net>
2024-10-10 21:08:26 +0300

    10. js-portfolio-125-web-dev. Scroll up. Active link on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
-/*=============== EMAIL JS ===============*/
-
-
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollup = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll') : scrollup.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass?.classList.add('active-link');
+        } else {
+            sectionsClass?.classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
